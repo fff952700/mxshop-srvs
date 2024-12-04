e6be04fa295645593d788455b0758843391995ab
fff952700 <fff952700@gmail.com>
2024-12-04 14:36:32 +0800

    brand

--- goods_srv/handler/brand.go
+++ goods_srv/handler/brand.go
@@ -2,10 +2,13 @@ package handler
 
 import (
 	"context"
-	"mxshop_srvs/goods_srv/model"
-	"mxshop_srvs/user_srv/global"
 
-	//"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	"mxshop_srvs/goods_srv/global"
+	"mxshop_srvs/goods_srv/model"
 	"mxshop_srvs/goods_srv/proto"
 )
 
@@ -19,8 +22,6 @@ func (g *GoodsServer) BrandList(ctx context.Context, req *proto.BrandFilterReque
 	if result.Error != nil {
 		return &brandListResponse, result.Error
 	}
-	// 获取总条数
-	brandListResponse.Total = int32(result.RowsAffected)
 	brandResponses := make([]*proto.BrandInfoResponse, 0)
 	for _, brand := range brands {
 		brandResponses = append(brandResponses, &proto.BrandInfoResponse{
@@ -29,15 +30,45 @@ func (g *GoodsServer) BrandList(ctx context.Context, req *proto.BrandFilterReque
 			Logo: brand.Logo,
 		})
 	}
+	var total int64
+	global.DB.Model(&model.Brands{}).Count(&total)
+	brandListResponse.Data = brandResponses
+	brandListResponse.Total = int32(total)
 	return &brandListResponse, nil
 }
 
-//func (g *GoodsServer) CreateBrand(ctx context.Context, req *proto.BrandRequest) (*proto.BrandInfoResponse, error){
-//
-//}
-//func (g *GoodsServer) DeleteBrand(ctx context.Context, req *proto.BrandRequest) (*emptypb.Empty, error){
-//
-//}
-//func (g *GoodsServer) UpdateBrand(ctx context.Context, req *proto.BrandRequest) (*emptypb.Empty, error){
-//
-//}
+func (g *GoodsServer) CreateBrand(ctx context.Context, req *proto.BrandRequest) (*proto.BrandInfoResponse, error) {
+	// 查询品牌是否存在
+	if result := global.DB.Where("name = ?", req.Name).First(&model.Brands{}); result.RowsAffected == 1 {
+		return nil, status.Errorf(codes.InvalidArgument, "商品已存在")
+	}
+	brand := &model.Brands{
+		Name: req.Name,
+		Logo: req.Logo,
+	}
+	global.DB.Create(brand)
+	return &proto.BrandInfoResponse{Id: brand.Id}, nil
+}
+
+func (g *GoodsServer) DeleteBrand(ctx context.Context, req *proto.BrandRequest) (*emptypb.Empty, error) {
+	if result := global.DB.Delete(&model.Brands{}, req.Id); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "商品不存在")
+	}
+	return &emptypb.Empty{}, nil
+
+}
+
+func (g *GoodsServer) UpdateBrand(ctx context.Context, req *proto.BrandRequest) (*emptypb.Empty, error) {
+	if result := global.DB.Where("name = ?", req.Id).First(&model.Brands{}); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "商品不存在")
+	}
+	var brand *model.Brands
+	if req.Name != "" {
+		brand.Name = req.Name
+	}
+	if req.Logo != "" {
+		brand.Name = req.Name
+	}
+	global.DB.Save(brand)
+	return &emptypb.Empty{}, nil
+}
