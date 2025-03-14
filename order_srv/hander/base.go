4d80cb2c6a96a6af40cf6fa065cee581ee6e0d5f
fff952700 <fff952700@gmail.com>
2025-03-14 10:49:10 +0800

    order

--- order_srv/hander/base.go
+++ order_srv/hander/base.go
@@ -1,6 +1,8 @@
 package hander
 
 import (
+	"fmt"
+	"gorm.io/gorm"
 	"mxshop_srvs/order_srv/model"
 	"mxshop_srvs/order_srv/proto"
 )
@@ -20,7 +22,42 @@ func (o *OrderServer) Model2InfoResponse(itemInterface interface{}) interface{} 
 			})
 		}
 		return cartItemList
+	// 订单
+	case []*model.OrderInfo:
+		var orderList []*proto.OrderResponse
+		for _, item := range itemInfo {
+			orderList = append(orderList, &proto.OrderResponse{
+				Id:      item.Id,
+				UserId:  item.UserId,
+				OrderSn: item.OrderSn,
+				PayType: item.PayType,
+				Status:  item.Status,
+				Post:    item.Post,
+				Address: item.Address,
+				Name:    item.SignerName,
+				Mobile:  item.SingerMobile,
+				PayTime: fmt.Sprintf("%T", item.PayTime),
+			})
+		}
+		return orderList
 	default:
 		return nil
+	}
+}
+
+func Paginate(page, pageSize int) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		if page <= 0 {
+			page = 1
+		}
+		switch {
+		case pageSize > 100:
+			pageSize = 100
+		case pageSize <= 0:
+			pageSize = 10
+		}
+
+		offset := (page - 1) * pageSize
+		return db.Offset(offset).Limit(pageSize)
 	}
 }
--- order_srv/hander/order.go
+++ order_srv/hander/order.go
@@ -2,6 +2,7 @@ package hander
 
 import (
 	"context"
+	"google.golang.org/protobuf/types/known/emptypb"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -44,10 +45,49 @@ func (o *OrderServer) CartItemCreate(ctx context.Context, req *proto.CartItemReq
 		cartItem.Checked = req.Checked
 		cartItem.GoodsId = req.GoodsId
 		cartItem.UserId = req.UserId
+		global.DB.Create(cartItem)
 	} else {
-		cartItem.Nums = req.Nums
-		cartItem.Checked = req.Checked
+		cartItem.Nums += req.Nums
+		global.DB.Where("id = ?", cartItem.Id).Updates(&model.ShoppingCart{Nums: cartItem.Nums, Checked: req.Checked})
 	}
-	global.DB.Save(cartItem)
 	return &proto.CartItemResponse{Id: cartItem.Id}, nil
+}
+
+func (o *OrderServer) CartItemUpdate(ctx context.Context, req *proto.CartItemRequest) (*emptypb.Empty, error) {
+	// 修改只能修改数量和选中状态 当没有传递值时 proto会使用默认值
+	var (
+		cartItem = &model.ShoppingCart{}
+	)
+	if result := global.DB.First(cartItem, req.Id); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "购物车记录不存在")
+	}
+	if req.Nums > 0 {
+		cartItem.Nums = req.Nums
+	}
+	cartItem.Checked = req.Checked
+	global.DB.Save(cartItem)
+	return &emptypb.Empty{}, nil
+}
+
+func (o *OrderServer) CartItemDelete(ctx context.Context, req *proto.CartItemRequest) (*emptypb.Empty, error) {
+	if result := global.DB.Delete(&model.ShoppingCart{}, req.Id); result.RowsAffected != 1 {
+		return nil, status.Errorf(codes.NotFound, "购物车记录不存在")
+	}
+	return &emptypb.Empty{}, nil
+}
+
+// 订单相关
+func (o *OrderServer) OrderList(ctx context.Context, req *proto.OrderFilterRequest) (*proto.OrderListResponse, error) {
+	// 通用查询。可能通过前端擦查询或者后台查询所有订单
+	var (
+		orderList []*model.OrderInfo
+		total     int64
+	)
+	global.DB.Where(&model.OrderInfo{UserId: req.UserId}).Count(&total)
+	global.DB.Scopes(Paginate(int(req.Page), int(req.PagePerNums))).Where(&model.OrderInfo{UserId: req.UserId}).Find(&orderList)
+	data := o.Model2InfoResponse(orderList)
+	return &proto.OrderListResponse{
+		Total: int32(total),
+		Data:  data.([]*proto.OrderResponse),
+	}, nil
 }
